9e655560a52baf549411b23792f06e3cda8fdaaa
Maria Le <hp.mars.le@gmail.com>
2020-11-04 12:46:17 -0600

    Implement useRef to access div to use CSS animation

--- src/Collapsible.js
+++ src/Collapsible.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Button } from '@material-ui/core'
-
+import './Collapsible.css'
 function Collapsible({ label, children }) {
   const [isOpen, setIsOpen] = useState(false)
+
+  const parentRef = useRef()
+
+  if (parentRef.current) console.log(parentRef.current.scrollHeight)
   return (
-    <div>
+    <div className='collapsible'>
       <Button
         className='toggle'
         variant='contained'
         color='primary'
         onClick={() => setIsOpen(!isOpen)}
+        style={{ marginBottom: '0.5rem' }}
       >
         {label}
       </Button>
-      {isOpen && <div className='content'>{children}</div>}
+      <div
+        className='content-parent'
+        ref={parentRef}
+        style={
+          isOpen
+            ? {
+                height: parentRef.current.scrollHeight + 'px',
+              }
+            : { height: '0px' }
+        }
+      >
+        <div className='content'>{children}</div>
+      </div>
     </div>
   )
 }
